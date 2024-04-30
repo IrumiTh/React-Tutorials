ac964c5d41daa2193f06589d9f4d90360ba82591
irumi <irumiabeywickrama@gmail.com>
2024-04-30 22:09:11 +0530

    showing an alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-
-
+import  { useState } from 'react';
+import Alerts from "./components/Alerts";
 import Button from "./components/Button";
 
-
-
 function App() {
+  const [showAlert, setShowAlert] = useState(false);
+
+  const handleClick = () => {
+    setShowAlert(true);
+  };
+
   return (
     <div>
-      <Button onClick={() =>console.log("Clicked")}>
+      <Button onClick={handleClick}>
         My button
       </Button>
+      {showAlert && <Alerts onClose={() => setShowAlert(false)}>Alert</Alerts>}
     </div>
   );
 }
--- src/components/Alerts.tsx
+++ src/components/Alerts.tsx
@@ -2,11 +2,13 @@ import { ReactNode } from "react";
 
 interface Props{
     children: ReactNode;
+    onClose: () => void;
 }  
-const Alerts = ({children}: Props) => {
+const Alerts = ({children, onClose}: Props) => {
   return (
-    <div className="alert alert-primary">
+    <div className="alert alert-primary alert-dismissible">
       {children}
+      <button type="button" className="btn-close" onClick={onClose} data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
   )
 }
