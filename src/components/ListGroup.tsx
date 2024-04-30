1fe736c4f532341efe2c1fe0a3e5ad85e9cd5487
irumi <irumiabeywickrama@gmail.com>
2024-04-30 11:08:46 +0530

    conditional rendeing

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,8 +1,10 @@
 function ListGroup() {
-    const items = ["New York", "London", "Paris", "Rome"];
+    let items = ["New York", "London", "Paris", "Rome"];
+    items = [];
   return (
     <>
         <h1>List Group</h1>
+        {items.length === 0 && <p>No item found</p>}
         <ul className="list-group list-group-flush">
           {items.map((item)=> <li key={item}>{item}</li>)}
         </ul>
